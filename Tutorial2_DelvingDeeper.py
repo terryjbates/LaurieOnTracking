6b34b2f3e50bf58462519f2d8aaf70e990028363
Terry Bates <terryjbates@gmail.com>
2026-01-11 01:04:49 -0800

    Added section for away sprinters

--- Tutorial2_DelvingDeeper.py
+++ Tutorial2_DelvingDeeper.py
@@ -13,6 +13,14 @@ GitHub repo: https://github.com/Friends-of-Tracking-Data-FoTD/LaurieOnTracking
 
 @author: Laurie Shaw (@EightyFivePoint)
 """
+import sys, os
+
+# set up initial path to data
+DATADIR = '/PATH/TO/WHERE/YOU/SAVED/THE/SAMPLE/DATA'
+DATADIR = r'C:\Users\lover\github\sample-data\data'
+
+sys.path.append(DATADIR)
+from importlib import reload  # Python 3.4+
 
 
 import Metrica_IO as mio
@@ -23,7 +31,7 @@ import numpy as np
 import pandas as pd
 
 # set up initial path to data
-DATADIR = '/PATH/TO/WHERE/YOU/SAVED/THE/SAMPLE/DATA'
+# DATADIR = '/PATH/TO/WHERE/YOU/SAVED/THE/SAMPLE/DATA'
 game_id = 2 # let's look at sample match 2
 
 # read in the event data
@@ -38,12 +46,14 @@ tracking_home = mio.to_metric_coordinates(tracking_home)
 tracking_away = mio.to_metric_coordinates(tracking_away)
 events = mio.to_metric_coordinates(events)
 
+# tracking_home.Period.idxmax(2)
+
 # reverse direction of play in the second half so that home team is always attacking from right->left
 tracking_home,tracking_away,events = mio.to_single_playing_direction(tracking_home,tracking_away,events)
 
 # Making a movie of the second home team goal
-#PLOTDIR = DATADIR
-#mviz.save_match_clip(tracking_home.iloc[73600:73600+500],tracking_away.iloc[73600:73600+500],PLOTDIR,fname='home_goal_2',include_player_velocities=False)
+PLOTDIR = DATADIR
+mviz.save_match_clip(tracking_home.iloc[73600:73600+500],tracking_away.iloc[73600:73600+500],PLOTDIR,fname='home_goal_2',include_player_velocities=False)
 
 # Calculate player velocities
 tracking_home = mvel.calc_player_velocities(tracking_home,smoothing=True)
@@ -57,7 +67,7 @@ tracking_away = mvel.calc_player_velocities(tracking_away,smoothing=True)
 # plot a random frame, plotting the player velocities using quivers
 mviz.plot_frame( tracking_home.loc[10000], tracking_away.loc[10000], include_player_velocities=True, annotate=True)
 
-# Create a Physical summary dataframe for home players
+# Create a Physical summary dataframe for home players.
 home_players = np.unique( [ c.split('_')[1] for c in tracking_home.columns if c[:4] == 'Home' ] )
 home_summary = pd.DataFrame(index=home_players)
 
@@ -78,6 +88,13 @@ for player in home_summary.index:
     player_distance = tracking_home[column].sum()/25./1000 # this is the sum of the distance travelled from one observation to the next (1/25 = 40ms) in km.
     distance.append( player_distance )
 home_summary['Distance [km]'] = distance
+
+# We notice that player 11 is lowest distance. Is he a goallkeeper?
+# plot a random frame, plotting the player velocities using quivers. He some-how
+# knows 51 is the frame to pick
+mviz.plot_frame( tracking_home.loc[51], tracking_away.loc[51], include_player_velocities=True, annotate=True)
+
+
 
 # make a simple bar chart of distance covered for each player
 plt.subplots()
@@ -107,7 +124,7 @@ for player in home_summary.index:
     # sprinting (greater than 7 m/s)
     player_distance = tracking_home.loc[ tracking_home[column] >= 7, column].sum()/25./1000
     sprinting.append( player_distance )
-    
+
 home_summary['Walking [km]'] = walking
 home_summary['Jogging [km]'] = jogging
 home_summary['Running [km]'] = running
@@ -129,7 +146,7 @@ for player in home_summary.index:
     player_sprints = np.diff( 1*( np.convolve( 1*(tracking_home[column]>=sprint_threshold), np.ones(sprint_window), mode='same' ) >= sprint_window ) )
     nsprints.append( np.sum( player_sprints == 1 ) )
 home_summary['# sprints'] = nsprints
-         
+
 # Plot the trajectories for each of player 10's sprints
 player = '10'
 column = 'Home_' + player + '_speed' # spped
@@ -144,5 +161,137 @@ fig,ax = mviz.plot_pitch()
 for s,e in zip(player_sprints_start,player_sprints_end):
     ax.plot(tracking_home[column_x].iloc[s],tracking_home[column_y].iloc[s],'ro')
     ax.plot(tracking_home[column_x].iloc[s:e+1],tracking_home[column_y].iloc[s:e+1],'r')
-    
+
+### Estimate player top speed
+# we have this in tracking_away and tracking_home
+
+import re
+
+for col in tracking_away.columns:
+    match = re.match('.*(\d)_speed$', col)
+    #if re.match('.*(\d)_speed$', col):
+    if match:
+        # print(f"match col: {col}\nmatched: player{match.group(1)}")
+        print(f"Maximum speed for Away player{match.group(1)} is",
+              f"{tracking_away[col].max():.2f} meters per second")
+
+
+for col in tracking_home.columns:
+    match = re.match('.*(\d)_speed$', col)
+    #if re.match('.*(\d)_speed$', col):
+    if match:
+        # print(f"match col: {col}\nmatched: player{match.group(1)}")
+        print(f"Maximum speed for Home player{match.group(1)} is",
+              f"{tracking_home[col].max():.2f} meters per second")
+
+# Compute acceleration for player 26 Away side
+
+dt = tracking_away["Time [s]"].diff()
+
+ax = tracking_away["Away_26_vx"].diff() / dt
+ay = tracking_away["Away_26_vy"].diff() / dt
+
+acc = np.sqrt(ax**2 + ay**2)
+# Filter for noise
+acc[acc > 11.0] = np.nan        # total accel cap
+# acc_long[acc_long > 7] = np.nan
+
+max_acc = acc.max()
+print(f"Max acceleraation of player 26 Away: {max_acc:.2f} meters per sec")
+
+
+
+
+
+# Create a Physical summary dataframe for away players.
+away_players = np.unique( [ c.split('_')[1] for c in tracking_away.columns if c[:4] == 'Away' ] )
+away_summary = pd.DataFrame(index=away_players)
+
+# Calculate minutes played for each player
+minutes = []
+for player in away_players:
+    # search for first and last frames that we have a position observation for each player (when a player is not on the pitch positions are NaN)
+    column = 'Away_' + player + '_x' # use player x-position coordinate
+    player_minutes = ( tracking_away[column].last_valid_index() - tracking_away[column].first_valid_index() + 1 ) / 25 / 60. # convert to minutes
+    minutes.append( player_minutes )
+away_summary['Minutes Played'] = minutes
+away_summary = away_summary.sort_values(['Minutes Played'], ascending=False)
+
+# Calculate total distance covered for each player
+distance = []
+for player in away_summary.index:
+    column = 'Away_' + player + '_speed'
+    player_distance = tracking_away[column].sum()/25./1000 # this is the sum of the distance travelled from one observation to the next (1/25 = 40ms) in km.
+    distance.append( player_distance )
+away_summary['Distance [km]'] = distance
+
+# We notice that player 11 is lowest distance. Is he a goallkeeper
+
+
+# make a simple bar chart of distance covered for each player
+plt.subplots()
+ax = away_summary['Distance [km]'].plot.bar(rot=0)
+ax.set_xlabel('Player')
+ax.set_ylabel('Distance covered [km]')
+
+
+# now calculate distance covered while: walking, joggings, running, sprinting
+walking = []
+jogging = []
+running = []
+sprinting = []
+for player in away_summary.index:
+    column = 'Away_' + player + '_speed'
+    # walking (less than 2 m/s)
+    player_distance = tracking_away.loc[tracking_away[column] < 2, column].sum()/25./1000
+    walking.append( player_distance )
+    # jogging (between 2 and 4 m/s)
+    player_distance = tracking_away.loc[ (tracking_away[column] >= 2) & (tracking_away[column] < 4), column].sum()/25./1000
+    jogging.append( player_distance )
+    # running (between 4 and 7 m/s)
+    player_distance = tracking_away.loc[ (tracking_away[column] >= 4) & (tracking_away[column] < 7), column].sum()/25./1000
+    running.append( player_distance )
+    # sprinting (greater than 7 m/s)
+    player_distance = tracking_away.loc[ tracking_away[column] >= 7, column].sum()/25./1000
+    sprinting.append( player_distance )
+
+away_summary['Walking [km]'] = walking
+away_summary['Jogging [km]'] = jogging
+away_summary['Running [km]'] = running
+away_summary['Sprinting [km]'] = sprinting
+
+# make a clustered bar chart of distance covered for each player at each speed
+ax = away_summary[['Walking [km]','Jogging [km]','Running [km]','Sprinting [km]']].plot.bar(colormap='coolwarm')
+ax.set_xlabel('Player')
+ax.set_ylabel('Distance covered [m]')
+
+# sustained sprints: how many sustained sprints per match did each player complete? Defined as maintaining a speed > 7 m/s for at least 1 second
+nsprints = []
+sprint_threshold = 7 # minimum speed to be defined as a sprint (m/s)
+sprint_window = 1*25 # minimum duration sprint should be sustained (in this case, 1 second = 25 consecutive frames)
+for player in away_summary.index:
+    column = 'Away_' + player + '_speed'
+    # trick here is to convolve speed with a window of size 'sprint_window', and find number of occassions that sprint was sustained for at least one window length
+    # diff helps us to identify when the window starts
+    player_sprints = np.diff( 1*( np.convolve( 1*(tracking_away[column]>=sprint_threshold), np.ones(sprint_window), mode='same' ) >= sprint_window ) )
+    nsprints.append( np.sum( player_sprints == 1 ) )
+away_summary['# sprints'] = nsprints
+
+# Plot the trajectories for each of player 10's sprints
+player = '26'
+column = 'Away_' + player + '_speed' # spped
+column_x = 'Away_' + player + '_x' # x position
+column_y = 'Away_' + player + '_y' # y position
+# same trick as before to find start and end indices of windows of size 'sprint_window' in which player speed was above the sprint_threshold
+player_sprints = np.diff( 1*( np.convolve( 1*(tracking_away[column]>=sprint_threshold), np.ones(sprint_window), mode='same' ) >= sprint_window ) )
+player_sprints_start = np.where( player_sprints == 1 )[0] - int(sprint_window/2) + 1 # adding sprint_window/2 because of the way that the convolution is centred
+player_sprints_end = np.where( player_sprints == -1 )[0] + int(sprint_window/2) + 1
+# now plot all the sprints
+fig,ax = mviz.plot_pitch()
+for s,e in zip(player_sprints_start,player_sprints_end):
+    ax.plot(tracking_away[column_x].iloc[s],tracking_away[column_y].iloc[s],'ro')
+    ax.plot(tracking_away[column_x].iloc[s:e+1],tracking_away[column_y].iloc[s:e+1],'r')
+
+
+
 # END
